5c186f5afc67e6fc724ae24603c131a93f8aa81c
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2018-02-06 21:00:55 +0100

    section 5: static files

--- section5/server.js
+++ section5/server.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
+
 // set up route for index
 app.get('/', (request, response) => {
-  response.send("Hello express!")
+  response.send({
+    name: 'Andrew',
+    likes: ['bikes', 'cities'],
+  })
+});
+
+app.get('/about', (req, resp) => {
+  resp.send('About page');
+});
+
+app.get('/bad', (req, resp) => {
+  resp.send({errorMessage: 'Unable to fulfill request'});
 });
 
 app.listen(3000);
