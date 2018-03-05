fffcf89ac3b484f48846d01ff63e6001b8502f6a
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2018-03-05 20:11:44 +0100

    Improve config handling

--- section7/server/config/config.js
+++ section7/server/config/config.js
@@ -2,10 +2,11 @@ const env = process.env.NODE_ENV || 'development';
 console.log(`Running in ${env} environment`);
 
 
-if (env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoApp';
-} else if (env == 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoAppTest';
+if (env !== 'production'){
+
+  const config = require('./config.json')[env];
+
+  Object.keys(config).forEach(key => {
+    process.env[key] = config[key];
+  })
 }
